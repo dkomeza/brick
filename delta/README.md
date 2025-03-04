12551078e1fe470724569ff704aecb653d0bdf0d
Dawid Komęza <dawid.komeza@gmail.com>
2025-03-04 20:59:00 +0100

    🎉 Add tests to the makefile

--- delta/README.md
+++ delta/README.md
@@ -10,5 +10,29 @@ Delta is the core analysis and move generation module for the Brick Bridge app. 
 - **AI-Powered Play**: Implements algorithms for intelligent decision-making.
 - **Performance Optimized**: Designed for speed and efficiency in C.
 
+## Running Delta
+
+### Prerequisites
+- Make
+- GCC
+
+### Build
+To build Delta, run the following command:
+```bash
+make
+```
+
+### Run
+To run Delta, execute the following command:
+```bash
+./delta
+```
+
+### Development
+To easily compile and run Delta, use the following command:
+```bash
+make run
+```
+
 ## License
 Delta is licensed under the GNU GPLv3 License. See LICENSE for details.
---
+++ delta/src/main.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef enum ACTION
+{
+  ACTION_NONE,
+
+  ACTION_GAME,
+  ACTION_ANALYZE,
+
+  ACTION_HELP,
+  ACTION_VERSION,
+} ACTION;
+
+ACTION parse_action(int argc, char **argv)
+{
+  if (argc == 1)
+  {
+    return ACTION_NONE;
+  }
+
+  if (argc == 2)
+  {
+    if (strcmp(argv[1], "game") == 0)
+    {
+      return ACTION_GAME;
+    }
+
+    if (strcmp(argv[1], "analyze") == 0)
+    {
+      return ACTION_ANALYZE;
+    }
+
+    if (strcmp(argv[1], "help") == 0)
+    {
+      return ACTION_HELP;
+    }
+
+    if (strcmp(argv[1], "version") == 0)
+    {
+      return ACTION_VERSION;
+    }
+  }
+
+  return ACTION_NONE;
+}
+
+int main(int argc, char **argv)
+{
+  ACTION action = parse_action(argc, argv);
+
+  switch (action)
+  {
+  case ACTION_GAME:
+    printf("Game\n");
+    break;
+  case ACTION_ANALYZE:
+    printf("Analyze\n");
+    break;
+  case ACTION_HELP:
+    printf("Help\n");
+    break;
+  case ACTION_VERSION:
+    printf("Version\n");
+    break;
+  default:
+    printf("Unknown action\n");
+    break;
+  }
+
+  return 0;
+}
